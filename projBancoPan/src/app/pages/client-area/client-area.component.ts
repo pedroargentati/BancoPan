dc7f770cdf83be7171fe7e7d773894db21f23b80
Yasmin Verderame <yasmin_verderame@hotmail.com>
2022-10-07 19:48:09 -0300

    add elementos

--- projBancoPan/src/app/pages/client-area/client-area.component.ts
+++ projBancoPan/src/app/pages/client-area/client-area.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-area.component.scss']
 })
 export class ClientAreaComponent implements OnInit {
+  public step: number = 1;
 
   constructor() { }
 
