e06d7ccba9bf1b4a5ac30fa6ae634d746b81001e
Yasmin Verderame <yasmin_verderame@hotmail.com>
2022-10-06 21:25:48 -0300

    fixing

--- projBancoPan/src/app/app.module.ts
+++ projBancoPan/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './pages/home/home.component';
-import { ModalProject1Component } from './components/modal-project1/modal-project1.component';
-import { ModalProject2Component } from './components/modal-project2/modal-project2.component';
-import { ModalProject3Component } from './components/modal-project3/modal-project3.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
@@ -16,9 +13,6 @@ import { CreateAccountComponent } from './pages/create-account/create-account.co
   declarations: [
     AppComponent,
     HomeComponent,
-    ModalProject1Component,
-    ModalProject2Component,
-    ModalProject3Component,
     HeaderComponent,
     CreateAccountComponent,
 
